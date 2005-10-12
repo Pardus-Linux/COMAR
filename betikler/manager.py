d492d59b7fa745f19a667fa899e3387d1e54608e
Gürer Özen <gurer@pardus.org.tr>
2005-10-12 09:09:52 +0000

    instalPackage çalışıyor, herhangi bir progress sırasında notification oluşturuyor, getRepositories de implement edildi

--- betikler/manager.py
+++ betikler/manager.py
@@ -14,17 +14,38 @@
 import pisi.api
 import pisi.installdb
 import pisi.packagedb
+import pisi.lockeddbshelve
+import pisi.ui
+
+class UI(pisi.ui.UI):
+    def confirm(self, msg):
+        return True
+    
+    def display_progress(self, pd):
+        #out = '\r%-30.30s %3d%% %12.2f %s' % \
+        #    (pd['filename'], pd['percent'], pd['rate'], pd['symbol'])
+        notify("System.Manager.progress", "%d" % pd['percent'])
+
+def _init_pisi():
+    ui = UI()
+    try:
+        pisi.api.init(ui=ui)
+    except pisi.lockeddbshelve.Error, e:
+        fail(str(e))
 
 def installPackage(package=None):
-    pisi.api.init()
+    _init_pisi()
     if package:
-        pisi.api.install(package)
+        try:
+            pisi.api.install([package])
+        except pisi.packagedb.Error, e:
+            return e
 
 def removePackage(package=None):
     return "NotImplemented"
 
 def updateIndex():
-    pisi.api.init()
+    _init_pisi()
     repos = pisi.api.ctx.repodb.list()
     if not repos:
         return "No package repository available"
@@ -36,7 +57,7 @@ def installCritical():
     return "NotImplemented"
 
 def getInstalled():
-    pisi.api.init()
+    _init_pisi()
     A = pisi.packagedb.inst_packagedb.list_packages()
     A.sort()
     B = map(lambda x: "%s %s %s" % (
@@ -44,8 +65,20 @@ def getInstalled():
     return "\n".join(B)
 
 def getUpgradable(type="all"):
-    pisi.api.init()
+    _init_pisi()
     A = pisi.api.list_upgradable()
     B = map(lambda x: "%s %s %s" % (
         x, pisi.packagedb.get_package(x).version, pisi.packagedb.get_package(x).release), A)
     return "\n".join(B)
+
+def getPackageInfo(package=None):
+    return "NotImplemented"
+
+def getRepositories():
+    _init_pisi()
+    A = pisi.api.ctx.repodb.list()
+    B = map(lambda x: "%s %s" % (x, pisi.api.ctx.repodb.get_repo(str(x)).indexuri.get_uri()), A)
+    return "\n".join(B)
+
+def setRepositories(repos=None):
+    return "NotImplemented"

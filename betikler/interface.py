82ee6d2afb8ffc02bcb79faf4eb167963e9dd07c
Onur Küçük <onur@pardus.org.tr>
2005-10-27 15:26:38 +0000

    hiç gökten karpuz düşer mi ?

--- betikler/interface.py
+++ betikler/interface.py
@@ -21,71 +21,23 @@ import csapi
 class ifconfig:
     """ ioctl stuff """
 
-#   From <bits/ioctls.h>
-
     IFNAMSIZ = 16               # interface name size
 
-    # Socket configuration controls
-    SIOCGIFNAME = 0x8910        # get iface name
-    SIOCSIFLINK = 0x8911        # set iface channel
+    # From <bits/ioctls.h>
+
+    SIOCGIFADDR = 0x8915        # get PA address
+    SIOCGIFBRDADDR  = 0x8919    # get broadcast PA address
     SIOCGIFCONF = 0x8912        # get iface list
     SIOCGIFFLAGS = 0x8913       # get flags
-    SIOCSIFFLAGS = 0x8914       # set flags
-    SIOCGIFADDR = 0x8915        # get PA address
-    SIOCSIFADDR = 0x8916        # set PA address
-    SIOCGIFDSTADDR  = 0x8917    # get remote PA address
-    SIOCSIFDSTADDR  = 0x8918    # set remote PA address
-    SIOCGIFBRDADDR  = 0x8919    # get broadcast PA address
-    SIOCSIFBRDADDR  = 0x891a    # set broadcast PA address
-    SIOCGIFNETMASK  = 0x891b    # get network PA mask
-    SIOCSIFNETMASK  = 0x891c    # set network PA mask
-    SIOCGIFMETRIC = 0x891d      # get metric
-    SIOCSIFMETRIC = 0x891e      # set metric
-    SIOCGIFMEM = 0x891f         # get memory address (BSD)
-    SIOCSIFMEM = 0x8920         # set memory address (BSD)
     SIOCGIFMTU = 0x8921         # get MTU size
+    SIOCGIFNETMASK  = 0x891b    # get network PA mask
+    SIOCSIFADDR = 0x8916        # set PA address
+    SIOCSIFBRDADDR  = 0x891a    # set broadcast PA address
+    SIOCSIFFLAGS = 0x8914       # set flags
     SIOCSIFMTU = 0x8922         # set MTU size
-    SIOCSIFNAME = 0x8923        # set interface name
-    SIOCSIFHWADDR = 0x8924      # set hardware address
-    SIOCGIFENCAP = 0x8925       # get/set encapsulations
-    SIOCSIFENCAP = 0x8926
-    SIOCGIFHWADDR = 0x8927      # Get hardware address
-    SIOCGIFSLAVE = 0x8929       # Driver slaving support
-    SIOCSIFSLAVE = 0x8930
-    SIOCADDMULTI = 0x8931       # Multicast address lists
-    SIOCDELMULTI = 0x8932
-    SIOCGIFINDEX = 0x8933       # name -> if_index mapping
-    SIOGIFINDEX = SIOCGIFINDEX  # misprint compatibility :-)
-    SIOCSIFPFLAGS = 0x8934      # set/get extended flags set
-    SIOCGIFPFLAGS = 0x8935
-    SIOCDIFADDR = 0x8936        # delete PA address
-    SIOCSIFHWBROADCAST = 0x8937 # set hardware broadcast addr
-    SIOCGIFCOUNT = 0x8938       # get number of devices
-    SIOCGIFBR = 0x8940          # Bridging support
-    SIOCSIFBR = 0x8941          # Set bridging options
-    SIOCGIFTXQLEN = 0x8942      # Get the tx queue length
-    SIOCSIFTXQLEN = 0x8943      # Set the tx queue length
+    SIOCSIFNETMASK  = 0x891c    # set network PA mask
 
-    # ARP cache control calls
-    SIOCDARP = 0x8953       # delete ARP table entry
-    SIOCGARP = 0x8954       # get ARP table entry
-    SIOCSARP = 0x8955       # set ARP table entry
-
-    # RARP cache control calls
-    SIOCDRARP = 0x8960      # delete RARP table entry
-    SIOCGRARP = 0x8961      # get RARP table entry
-    SIOCSRARP = 0x8962      # set RARP table entry
-
-    # Driver configuration calls
-    SIOCGIFMAP = 0x8970     # Get device parameters
-    SIOCSIFMAP = 0x8971     # Set device parameters
-
-    # DLCI configuration calls
-    SIOCADDDLCI = 0x8980   # Create new DLCI device
-    SIOCDELDLCI = 0x8981   # Delete DLCI device
-
-
-#   From <net/if.h>    
+    # From <net/if.h>    
 
     IFF_UP = 0x1           # Interface is up.
     IFF_BROADCAST = 0x2    # Broadcast address valid.
@@ -259,13 +211,11 @@ class ifconfig:
 class route:
     """ ioctl stuff """
 
-#   From <bits/ioctls.h>
+    # From <bits/ioctls.h>
 
-    # Routing table calls
     SIOCADDRT = 0x890B      # add routing table entry
     SIOCDELRT = 0x890C      # delete routing table entry
     SIOCRTMSG = 0x890D      # call to routing system
-
     INADDR_ANY = '\0' * 4   # Any Internet Address
 
     def delRoute(self, gw, dst = "0.0.0.0", mask = "0.0.0.0"):

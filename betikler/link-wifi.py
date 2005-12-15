17c843ef438e8adabd3bc1b0822ee5b559b41ebd
Gürer Özen <gurer@pardus.org.tr>
2005-12-15 04:43:17 +0000

    dhcp gene

--- betikler/link-wifi.py
+++ betikler/link-wifi.py
@@ -15,6 +15,8 @@ import fcntl
 import struct
 import socket
 import csapi
+import popen2
+from glob import glob
 
 class ifconfig:
     """ ioctl stuff """
@@ -361,6 +363,52 @@ class Wireless:
             return None
 
 
+class Dhcp:
+    def _run(self, args):
+        cmd = "/sbin/dhcpcd " + args
+        a = popen2.Popen4(cmd)
+
+        return a.wait() 
+
+    def start(self, ifname, timeout = "30"):
+        """ Start the DHCP client daemon """
+        # Maybe we should leave this to GUI
+        if ifname in self.getRunning():
+            self.stop(ifname)
+
+        # -R -Y -N to prevent dhcpcd rewrite nameservers
+        #          we should add nameservers, not rewrite them
+        # -H to set hostname due to info from server
+        # -t for timeout
+
+        args = "-R -Y -N -H -t " + timeout + " " + ifname
+        return self._run(args)
+
+    def stop(self, ifname):
+        """ Stop DHCP client daemon """
+        args = "-k " + ifname
+        return self._run(args)
+
+    def getNameServers(self, ifname):
+        """ Get DNS server list provided by the server """
+        info_file = "/var/lib/dhcpc/dhcpcd-" + ifname + ".info"
+
+        try:
+            f = file(info_file)
+            for line in f.readlines():
+                if not line.find("DNS="):
+                    return line[line.find("DNS=")+4:].rstrip('\n').split(',')
+            f.close()
+        except IOError:
+            return "Could not open file" # FIXME: put an error message here
+
+    def getRunning(self):
+        d = []
+        for i in glob("/var/run/dhcpcd-*.pid"):
+            d.append(i.rstrip(".pid").lstrip("/var/run/dhcpcd-"))
+        return d
+
+
 def sysValue(path, dir, file_):
     f = file(os.path.join(path, dir, file_))
     data = f.read().rstrip('\n')
@@ -444,16 +492,20 @@ class Dev:
         self.mask = _get(dict, "mask", None)
     
     def up(self):
-        ifc = ifconfig()
         if self.remote:
             wifi = Wireless()
             wifi.setEssid(self.dev, self.remote)
-        if self.address:
-            ifc.setAddr(self.dev, self.address)
-        ifc.setStatus(self.dev, "UP")
-        if self.gateway:
-            route = Route()
-            route.setDefaultRoute(self.gateway)
+        if self.mode == "manual":
+            ifc = ifconfig()
+            if self.address:
+                ifc.setAddr(self.dev, self.address)
+            ifc.setStatus(self.dev, "UP")
+            if self.gateway:
+                route = Route()
+                route.setDefaultRoute(self.gateway)
+        else:
+            dd = Dhcp()
+            print dd.start(self.dev, timeout="5")
         notify("Net.Link.stateChanged", self.name + "\nup")
     
     def down(self):
@@ -541,7 +593,7 @@ def getAddress(name=None):
 
     if dev.mode == "auto":
         # FIXME: query interface
-        s = "\n".join([name, dev.mode, "fixme"])
+        s = "\n".join([name, dev.mode, '', ''])
     else:
         s = "\n".join([name, dev.mode, dev.address, dev.gateway])
         if dev.mask:

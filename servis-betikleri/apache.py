6fea1155cced02411511c1c6a719c222ce5d7951
Ahmet Aygün <ahmet@pardusman.org>
2006-03-25 14:35:44 +0000

    Kim dedi gereksiz diye?

--- servis-betikleri/apache.py
+++ servis-betikleri/apache.py
@@ -5,8 +5,10 @@ serviceType = "server"
 serviceDesc = "Apache Web Server"
 
 def start():
-    run("/usr/sbin/apachec2tl", "start")
+    run("/usr/sbin/apache2ctl", "-d", "/usr/lib/apache2/", "-f", "/etc/apache2/httpd.conf", get_config_vars(), "-k", "start")
 
 def stop():
-    run("/usr/sbin/apachec2tl", "stop")
+    run("/usr/sbin/apache2ctl", "-d", "/usr/lib/apache2/", "-f", "/etc/apache2/httpd.conf", get_config_vars(), "-k", "stop")
 
+def get_config_vars():
+    return map(lambda x: x.split('=')[1].strip().strip('"'), [line for line in open('/etc/conf.d/apache2').readlines() if line.strip().startswith('APACHE2_OPTS')])[0]

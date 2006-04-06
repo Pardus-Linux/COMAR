cfd745d2e4fccd6038133e818d5079bbc15e6dee
Ali Erdinç Köroğlu <erdinc@pardus.org.tr>
2006-04-06 07:13:19 +0000

    bunlar şimdilik pasifize olsunlar pasifik okyanusu passive ftp multipass pas ver

--- servis-betikleri/register_scripts.sh
+++ servis-betikleri/register_scripts.sh
@@ -11,8 +11,6 @@ hav register System.Service local `pwd`/local.py
 hav register System.Service mdnsd `pwd`/mdnsd.py
 hav register System.Service sysklogd `pwd`/sysklogd.py
 hav register System.Service openssh `pwd`/ssh.py
-hav register System.Service portmap `pwd`/portmap.py
 hav register System.Service powersave `pwd`/powersave.py
-hav register System.Service tftp `pwd`/tftp.py
 hav register System.Service xorg `pwd`/xdm.py
 hav register System.Service zemberek-server `pwd`/zemberek.py

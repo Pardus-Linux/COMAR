1e3df8d78b312fff6cfcdfa8c95d387d2b1c2d6b
Ali Erdinç Köroğlu <erdinc@pardus.org.tr>
2006-04-07 11:00:12 +0000

    dede

---
+++ servis-betikleri/dhcp.py
@@ -0,0 +1,11 @@
+import os
+from comar.service import *
+
+serviceType = "server"
+serviceDesc = "DHCP Daemon"
+
+def start():
+    run("start-stop-daemon --start --exec /usr/sbin/dhcpd --p /var/run/dhcp/dhcpd.pid -u dhcp -g dhcp eth0")
+
+def stop():
+    run("start-stop-daemon --stop --exec /usr/sbin/dhcpd --p /var/run/dhcp/dhcpd.pid")
--- servis-betikleri/tftp.py
+++ servis-betikleri/tftp.py
@@ -5,7 +5,7 @@ serviceType = "server"
 serviceDesc = "TFTP Daemon"
 
 def start():
-    run("/usr/sbin/in.tftpd -l")
+    run("/usr/sbin/in.tftpd -s -l /tftpboot")
 
 def stop():
     run("start-stop-daemon --stop --exec /usr/sbin/in.tftpd")

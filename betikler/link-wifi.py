c3576922ecd88209fb5921670931de0819019e3a
Gürer Özen <gurer@pardus.org.tr>
2006-08-17 18:35:41 +0000

    sen de sadeleş

--- betikler/link-wifi.py
+++ betikler/link-wifi.py
@@ -19,6 +19,7 @@ import csapi
 import popen2
 from glob import glob
 from comar.device import idsQuery
+from comar import network
 
 def capture(cmd):
     out = []
@@ -29,198 +30,6 @@ def capture(cmd):
             break
         out.append(b)
     return (a.wait(), out)
-
-
-class ifconfig:
-    """ ioctl stuff """
-
-    IFNAMSIZ = 16               # interface name size
-
-    # From <bits/ioctls.h>
-
-    SIOCGIFADDR = 0x8915        # get PA address
-    SIOCGIFBRDADDR  = 0x8919    # get broadcast PA address
-    SIOCGIFCONF = 0x8912        # get iface list
-    SIOCGIFFLAGS = 0x8913       # get flags
-    SIOCGIFMTU = 0x8921         # get MTU size
-    SIOCGIFNETMASK  = 0x891b    # get network PA mask
-    SIOCSIFADDR = 0x8916        # set PA address
-    SIOCSIFBRDADDR  = 0x891a    # set broadcast PA address
-    SIOCSIFFLAGS = 0x8914       # set flags
-    SIOCSIFMTU = 0x8922         # set MTU size
-    SIOCSIFNETMASK  = 0x891c    # set network PA mask
-
-    # From <net/if.h>    
-
-    IFF_UP = 0x1           # Interface is up.
-    IFF_BROADCAST = 0x2    # Broadcast address valid.
-    IFF_DEBUG = 0x4        # Turn on debugging.
-    IFF_LOOPBACK = 0x8     # Is a loopback net.
-    IFF_POINTOPOINT = 0x10 # Interface is point-to-point link.
-    IFF_NOTRAILERS = 0x20  # Avoid use of trailers.
-    IFF_RUNNING = 0x40     # Resources allocated.
-    IFF_NOARP = 0x80       # No address resolution protocol.
-    IFF_PROMISC = 0x100    # Receive all packets.
-    IFF_ALLMULTI = 0x200   # Receive all multicast packets.
-    IFF_MASTER = 0x400     # Master of a load balancer.
-    IFF_SLAVE = 0x800      # Slave of a load balancer.
-    IFF_MULTICAST = 0x1000 # Supports multicast.
-    IFF_PORTSEL = 0x2000   # Can set media type.
-    IFF_AUTOMEDIA = 0x4000 # Auto media select active.
-
-
-    def __init__(self):
-        # create a socket to communicate with system
-        self.sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    def _ioctl(self, func, args):
-        return fcntl.ioctl(self.sockfd.fileno(), func, args)
-
-    def _call(self, ifname, func, ip = None):
-
-        if ip is None:
-            data = (ifname + '\0'*32)[:32]
-        else:
-            ifreq = (ifname + '\0' * self.IFNAMSIZ)[:self.IFNAMSIZ]
-            data = struct.pack("16si4s10x", ifreq, socket.AF_INET, socket.inet_aton(ip))
-
-        try:
-            result = self._ioctl(func, data)
-        except IOError:
-            return None
-
-        return result
-
-    def _readsys(self, ifname, f):
-        try:
-            fp = file(os.path.join("/sys/class/net", ifname, f))
-            result = fp.readline().rstrip('\n')
-            fp.close()
-        except IOError:
-            return None
-            
-        return result
-
-    def getInterfaceList(self):
-        """ Get all interface names in a list """
-        # get interface list
-        buffer = array.array('c', '\0' * 1024)
-        ifconf = struct.pack("iP", buffer.buffer_info()[1], buffer.buffer_info()[0])
-        result = self._ioctl(self.SIOCGIFCONF, ifconf)
-
-        # loop over interface names
-        iflist = []
-        size, ptr = struct.unpack("iP", result)
-        for idx in range(0, size, 32):
-            ifconf = buffer.tostring()[idx:idx+32]
-            name, dummy = struct.unpack("16s16s", ifconf)
-            name, dummy = name.split('\0', 1)
-            iflist.append(name)
-
-        return iflist
-
-    def getAddr(self, ifname):
-        """ Get the inet addr for an interface """
-        result = self._call(ifname, self.SIOCGIFADDR)
-        return socket.inet_ntoa(result[20:24])
-
-    def getNetmask(self, ifname):
-        """ Get the netmask for an interface """
-        result = self._call(ifname, self.SIOCGIFNETMASK)
-        return socket.inet_ntoa(result[20:24])
-
-    def getBroadcast(self, ifname):
-        """ Get the broadcast addr for an interface """
-        result = self._call(ifname, self.SIOCGIFBRDADDR)
-        return socket.inet_ntoa(result[20:24])
-
-    def getStatus(self, ifname):
-        """ Check whether interface is UP """
-        result = self._call(ifname, self.SIOCGIFFLAGS)
-        flags, = struct.unpack('H', result[16:18])
-        return (flags & self.IFF_UP) != 0
-
-    def getMTU(self, ifname):
-        """ Get the MTU size of an interface """
-        data = self._call(ifname, self.SIOCGIFMTU)
-        mtu = struct.unpack("16si12x", data)[1]
-        return mtu
-
-    def getMAC(self, ifname):
-        """ Get MAC address of an interface """
-        mac = self._readsys(ifname, "address")
-        return mac
-
-    def getRX(self, ifname):
-        """ Get received bytes of an interface """
-        rx = self._readsys(ifname, "statistics/rx_bytes")
-        return int(rx)
-
-    def getTX(self, ifname):
-        """ Get transferred bytes of an interface """
-        tx = self._readsys(ifname, "statistics/tx_bytes")
-        return int(tx)
-
-    def setAddr(self, ifname, ip):
-        """ Set the inet addr for an interface """
-        result = self._call(ifname, self.SIOCSIFADDR, ip)
-
-        if result and socket.inet_ntoa(result[20:24]) is ip:
-            return True
-        else:
-            return None
-
-    def setNetmask(self, ifname, ip):
-        """ Set the netmask for an interface """
-        result = self._call(ifname, self.SIOCSIFNETMASK, ip)
-
-        if result and socket.inet_ntoa(result[20:24]) is ip:
-            return True
-        else:
-            return None
-
-    def setBroadcast(self, ifname, ip):
-        """ Set the broadcast addr for an interface """
-        result = self._call(ifname, self.SIOCSIFBRDADDR, ip)
-
-        if socket.inet_ntoa(result[20:24]) is ip:
-            return True
-        else:
-            return None
-
-    def setStatus(self, ifname, status):
-        """ Set interface status (UP/DOWN) """
-        ifreq = (ifname + '\0' * self.IFNAMSIZ)[:self.IFNAMSIZ]
-
-        if status is "UP":
-            flags = self.IFF_UP
-            flags |= self.IFF_RUNNING
-            flags |= self.IFF_BROADCAST
-            flags |= self.IFF_MULTICAST
-            flags &= ~self.IFF_NOARP
-            flags &= ~self.IFF_PROMISC
-        elif status is "DOWN":
-            result = self._call(ifname, self.SIOCGIFFLAGS)
-            flags, = struct.unpack('H', result[16:18])
-            flags &= ~self.IFF_UP
-        else:
-            return None
-
-        data = struct.pack("16sh", ifreq, flags)
-        result = self._ioctl(self.SIOCSIFFLAGS, data)
-        return result
-
-    def setMTU(self, ifname, mtu):
-        """ Set the MTU size of an interface """
-        ifreq = (ifname + '\0' * self.IFNAMSIZ)[:self.IFNAMSIZ]
-
-        data = struct.pack("16si", ifreq, mtu)
-        result = self._ioctl(self.SIOCSIFMTU, data)
-
-        if struct.unpack("16si", result)[1] is mtu:
-            return True
-        else:
-            return None
 
 
 class Route:
@@ -449,12 +258,12 @@ class Scanner:
         return ""
     
     def scan(self, ifc):
-        icfg = ifconfig()
-        if not icfg.getStatus(ifc):
+        icfg = network.IF(ifc)
+        if not icfg.isUp():
             # Some wireless drivers cant do the scan while
             # interface is down, doh :(
-            icfg.setAddr(ifc, "0.0.0.0")
-            icfg.setStatus(ifc, "UP")
+            icfg.setAddress("0.0.0.0")
+            icfg.up()
         a = capture("/usr/sbin/iwlist %s scanning" % ifc)
         re.sub('ESSID:.*"(.*)"', self._collect, "\n".join(a[1]))
         return "\n".join(self.list)
@@ -560,24 +369,20 @@ class Dev:
             os.system("/usr/sbin/iwconfig %s enc restricted %s" % (self.dev, self.password))
         else:
             os.system("/usr/sbin/iwconfig %s enc off" % (self.dev))
+        ifc = network.IF(self.dev)
         if self.mode == "manual":
-            ifc = ifconfig()
-            if self.address:
-                ifc.setAddr(self.dev, self.address)
-            if self.mask:
-                ifc.setNetmask(self.dev, self.mask)
-            ifc.setStatus(self.dev, "UP")
+            ifc.setAddress(self.address, self.mask)
+            ifc.up()
             if self.gateway:
                 route = Route()
                 route.setDefaultRoute(self.gateway)
             notify("Net.Link.stateChanged", self.name + "\nup")
         else:
-            ifc = ifconfig()
             dd = Dhcp()
             notify("Net.Link.stateChanged", self.name + "\nconnecting")
             dd.start(self.dev, timeout="20")
-            if ifc.getStatus(self.dev):
-                addr = ifc.getAddr(self.dev)
+            if ifc.isUp():
+                addr = ifc.getAddress()[0]
                 notify("Net.Link.connectionChanged", "gotaddress " + self.name + "\n" + unicode(addr))
                 notify("Net.Link.stateChanged", self.name + "\nup")
             else:
@@ -588,8 +393,8 @@ class Dev:
         if self.mode != "manual":
             dd = Dhcp()
             dd.stop(self.dev)
-        ifc = ifconfig()
-        ifc.setStatus(self.dev, "DOWN")
+        ifc = network.IF(self.dev)
+        ifc.down()
         notify("Net.Link.stateChanged", self.name + "\ndown")
 
 

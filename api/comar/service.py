2f6c4d9e93c080acee8fbcf9721afe0f6f1d44a9
Gürer Özen <gurer@pardus.org.tr>
2007-05-16 14:39:52 +0000

    şimdi bişeye benziyor.

--- api/comar/service.py
+++ api/comar/service.py
@@ -10,6 +10,10 @@
 #
 
 import os
+import subprocess
+import fcntl
+import termios
+import pwd
 
 from comar.utility import *
 
@@ -83,7 +87,118 @@ class Config(dict):
 
 config = Config()
 
-# default methods
+# Service control utilities
+
+def _getPid(pidfile):
+    """Read process ID from a .pid file."""
+    try:
+        pid = file(pidfile).read()
+    except IOError, e:
+        if e.errno != 2:
+            raise
+        return None
+    # Some services put custom data after the first line
+    pid = pid.split("\n")[0]
+    # Non-pid data is also seen when stopped state in some services :/
+    if len(pid) == 0 or len(filter(lambda x: not x in "0123456789", pid)) > 0:
+        return None
+    return int(pid)
+
+def _checkPid(pid, user_uid=None, command=None):
+    """Check that given process ID matches our criteria."""
+    path = "/proc/%d" % pid
+    # Check that process is running
+    if not os.path.exists(path):
+        return False
+    # Check that process belongs to correct user
+    if user_uid:
+        st = os.stat(path)
+        if st.st_uid != user_uid:
+            return False
+    # Check that process is an instance of the correct binary
+    # FIXME: how to do that without restart problems???
+    return True
+
+def _findProcesses(command=None, pidfile=None, user=None):
+    """Return the list of process IDs matching our criteria."""
+    pids = []
+    user_uid = None
+    if user:
+        pw = pwd.getpwnam(user)
+        user_uid = pw.pw_uid
+    for entry in os.listdir("/proc"):
+        if entry[0] in "0123456789":
+            pid = int(entry)
+            if _checkPid(pid, user_uid=user_uid):
+                pids.append(pid)
+    if len(pids) > 0:
+        return pids
+    return None
+
+# Service control API
+
+def startService(command, args=None, pidfile=None, makepid=False, nice=None, detach=False):
+    """Start given service.
+    
+    command:  Path to the service executable.
+    args:     Optional arguments to the service executable.
+    pidfile:  Process ID of the service is kept in this file when running.
+    nice:     This value is added to the service process' niceness value, which
+              decreases its scheduling priority.
+    detach:   If the service doesn't detach on its own, this option will fork
+              and run it in the background.
+    makepid:  Write the pid file if service does not create on its own. Mostly useful
+              with the detach option.
+    """
+    cmd = [ command ]
+    if args:
+        if isinstance(args, basestring):
+            args = args.split()
+        cmd.extend(args)
+    
+    # FIXME: check if service is already running
+    
+    def fork_handler():
+        if nice is not None:
+            os.nice(nice)
+        if detach:
+            # Set umask to a sane value
+            # (other and group has no write permission by default)
+            os.umask(022)
+            # Detach from controlling terminal
+            tty_fd = os.open("/dev/tty", os.O_RDWR)
+            fcntl.ioctl(tty_fd, termios.TIOCNOTTY)
+            os.close(tty_fd)
+            # Close IO channels
+            devnull_fd = os.open("/dev/null", os.O_RDWR)
+            os.dup2(devnull_fd, 0)
+            os.dup2(devnull_fd, 1)
+            os.dup2(devnull_fd, 2)
+            # Detach from process group
+            os.setsid()
+        if makepid and pidfile:
+            file(pidfile, "w").write("%d\n" % os.getpid())
+        # FIXME: support chuid
+    
+    popen = subprocess.Popen(cmd, close_fds=True, preexec_fn=fork_handler)
+    if not detach:
+        print popen.wait()
+
+def stopService():
+    """Stop given service."""
+    pass
+    # FIXME: implement this
+
+def isServiceRunning(pidfile):
+    """Return if given service is currently running."""
+    pid = _getPid(pidfile)
+    if pid is None:
+        return False
+    if not _checkPid(pid):
+        return False
+    return True
+
+# Default Comar class methods
 
 def info():
     from csl import serviceType

87252690c4e98be2fafbbec398f174a98bd49019
Bahadır Kandemir <bahadir@pardus.org.tr>
2007-09-22 19:47:34 +0000

    tidy

--- ahenk/ajan/ajan/mod_service.py
+++ ahenk/ajan/ajan/mod_service.py
@@ -1,15 +1,24 @@
- #G.Selda Kuruoglu
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version. Please read the COPYING file.
+#
 
-import comar                        # for 'comar's connection functions 
-import logging                      # for log messages
-import ajan.ldaputil                
+import comar
+import logging
+import ajan.ldaputil
 
 
 class ServicePolicy(ajan.ldaputil.LdapClass):
     """ Service policy class has 2 attributes : service names to start, service names to stop """    
     entries = (
-        ("service_to_start", "comarServiceStart", set, set()),
-        ("service_to_stop", "comarServiceStop", set, set()),
+        ("start", "comarServiceStart", set, set()),
+        ("stop", "comarServiceStop", set, set()),
     )
 
 
@@ -17,51 +26,58 @@ class Policy:
     def __init__(self):
         self.policy = ServicePolicy()
         self.log = logging.getLogger("Mod.Service")
+    
     def override(self, attr, is_ou = False):
         """ Overrides service policy"""
         temp = ServicePolicy(attr)
         
         # Retrieve current service policy
-        start_set = temp.service_to_start
-        stop_set = temp.service_to_stop
-
+        start_set = temp.start
+        stop_set = temp.stop
+        
         if is_ou:
-            start_set.union (self.policy.service_to_start)
-            stop_set.union (self.policy.service_to_stop)
+            start_set.union(self.policy.start)
+            stop_set.union(self.policy.stop)
         
         else:
-            start_set = start_set.union (self.policy.service_to_start)
-            start_set = start_set.difference (stop_set)
-
-            stop_set = stop_set.union (self.policy.service_to_stop)
-            stop_set = stop_set.difference (start_set)
-
-        self.policy.service_to_start = start_set
-        self.policy.service_to_stop = stop_set
-
-    def update(self, computer, units ):
+            start_set = start_set.union(self.policy.start)
+            start_set = start_set.difference(stop_set)
+            
+            stop_set = stop_set.union(self.policy.stop)
+            stop_set = stop_set.difference(start_set)
+        
+        self.policy.start = start_set
+        self.policy.stop = stop_set
+    
+    def update(self, computer, units):
         """ Updates service policy"""
-        self.log.debug("Updating Service Policy")             
+        self.log.debug("Updating Service Policy")
         self.policy = ServicePolicy()
         for unit in units:
             self.override(unit, True)
-        self.override(computer)                    
+        self.override(computer)
         self.log.debug("Service policy is now:\n%s" % str(self.policy))
     
-    
-    def start_service (self):
-        """ starts sevices in 'service_to_start' list by connecting comar """
-        link = comar.Link()
-        for service in self.policy.service_to_start:
-            link.System.Service[service].start()
-
-    def stop_service (self):
-        """ stops sevices in 'service_to_start' list by connecting comar """
-        link = comar.Link()
-        for service in self.policy.service_to_stop:
-            link.System.Service[service].stop()
-
     def apply(self):
         self.log.debug("Applying Service Policy" )
-        self.start_service()
-        self.stop_service()
+        link = comar.Link()
+        
+        def serviceSetState(service, state):
+            link.System.Service[service].setState(state=state)
+            link.read_cmd()
+        
+        def serviceStart(service):
+            link.System.Service[service].start()
+            link.read_cmd()
+        
+        def serviceStop(service):
+            link.System.Service[service].stop()
+            link.read_cmd()
+        
+        for service in self.policy.stop:
+            serviceSetState(service, "off")
+            serviceStop(service)
+        
+        for service in self.policy.start:
+            serviceSetState(service, "on")
+            serviceStart(service)
